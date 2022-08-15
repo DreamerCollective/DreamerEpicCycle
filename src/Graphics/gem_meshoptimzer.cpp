47b3d5066bc77ee9b1489060544c52fd72376f87
Zalron <davidrlawson@optusnet.com.au>
2022-08-15 21:20:10 +1000

    Added mesh optimzer code as example for nanite like rendering

---
+++ src/Graphics/gem_meshoptimzer.cpp
@@ -0,0 +1,20 @@
+
+
+#include "gem_meshoptimzer.h"
+
+void gem_meshoptimzer::Meshoptimzer()
+{
+//    Mesh mesh;
+//    if(mesh.IodCount < COUNTOF(mesh.Lods))
+//    {
+//        size_t nextIndicesTarget = size_t(double(LodIndices.size()) * 0.75);
+//        size_t nextIndicesTarget = Meshopt_simplify(lodIndices.data(), lodIndeices.data(), lodIndices.size(). &vertices[0].vx, vertices.Size(), sizeof(Vertex), nextIndicesTarget, 1e-2f);
+//        assert(nextIndices <= lodIndices.size());
+//
+//        if(nextIndices == IodIndieces.size())
+//            break;
+//
+//        LodIndices.resize(nextIndices)
+//        meshopt_optimzeVertexCache(LodIndices.data(), LodIndices.data(), LodIndices.size(), vertex_count)
+//    }
+}
---
+++ src/Graphics/gem_meshoptimzer.h
@@ -0,0 +1,15 @@
+#include std
+
+struct Mesh
+{
+    int IodCount;
+    int Lods;
+    V
+};
+
+class gem_meshoptimzer {
+public:
+    void Meshoptimzer();
+};
+
+
